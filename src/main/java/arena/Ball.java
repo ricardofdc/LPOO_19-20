808d452e3523b52ce3ac3b83c5b8ba5e33f0a20c
Ricardo Cardoso <up201604686@fe.up.pt>
2020-04-23 17:55:25 +0100

    ball direction added

--- src/main/java/arena/Ball.java
+++ src/main/java/arena/Ball.java
@@ -1,7 +1,31 @@
 package arena;
 
 public class Ball extends Element {
+    private Direction dir;
+
     public Ball(int x, int y) {
         super(x, y);
+        dir = Direction.UP;
     }
+
+    private void changeDirectionSymmetric(){
+        switch (dir){
+            case UP:
+                break;
+            case LEFT1:
+                dir = Direction.RIGHT1;
+                break;
+            case LEFT2:
+                dir = Direction.RIGHT2;
+                break;
+            case RIGHT1:
+                dir = Direction.LEFT1;
+                break;
+            case RIGHT2:
+                dir = Direction.LEFT2;
+                break;
+        }
+    }
+
+    
 }
---
+++ src/main/java/arena/Direction.java
@@ -0,0 +1,17 @@
+package arena;
+
+public enum Direction {
+    UP,
+    LEFT1,
+    LEFT2,
+    RIGHT1,
+    RIGHT2
+}
+
+/*
+  UP  || LEFT1 || LEFT2 || RIGHT1 || RIGHT2
+..o.. || .o... || o.... || ...o.  || ....o
+..o.. || ..o.. || ..o.. || ..o..  || ..o..
+..o.. || ...o. || ....o || .o...  || o....
+
+ */
