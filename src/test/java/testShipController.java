b47f82e96ad0c1a9d8719c4f641e86c1bfc7b5d9
Marta Lobo <56152625+martandradelobo@users.noreply.github.com>
2020-05-30 17:27:59 +0100

    mais testes funcionais

--- src/test/java/testShipController.java
+++ src/test/java/testShipController.java
@@ -1,23 +1,34 @@
 import Model.Position;
 import Controller.ShipController;
+import Model.Ship;
 import org.junit.*;
+
+import java.awt.font.LineBreakMeasurer;
+
 import static org.junit.Assert.*;
 
 public class testShipController
 {
+    Ship ship;
+
+    @Before
+    public void prepare(){
+        ship = new Ship(new Position(1,1));
+    }
+
     @Test
     public void testShipMoveLeft()
     {
-        ShipController ship = new ShipController(new Position(1,1));
         Position pos = new Position(0,1);
-        assertEquals(ship.moveShipLeft().getX(), pos.getX());
+        assertEquals(ship.getPosition().left(), pos);
     }
 
     @Test
     public void testShipMoveRight()
     {
-        ShipController ship = new ShipController(new Position(1,1));
         Position pos = new Position(2,1);
-        assertEquals(ship.moveShipRight().getX(), pos.getX());
+        assertEquals(ship.getPosition().right(), pos);
     }
+
+
 }
