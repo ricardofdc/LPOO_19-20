e814b922b96a987d1c24828035401e691ab3f988
Marta Lobo <56152625+martandradelobo@users.noreply.github.com>
2020-05-30 16:07:14 +0100

    test element e position a passar

--- src/test/java/testPosition.java
+++ src/test/java/testPosition.java
@@ -1,26 +1,44 @@
 import Model.Position;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class testPosition
-{
-    @Test
-    public void testGetPos()
-    {
-        Position position = new Position(1,2);
 
-        assertEquals(position.getX(), 1);
-        assertEquals(position.getY(), 2);
+public class testPosition {
+    Position pos;
+
+    @Before
+    public void prepare(){
+        pos = new Position(10,10);
     }
 
     @Test
-    public void testEquals()
-    {
-        Position pos1 = new Position(1,2);
-        Position pos2 = new Position(4,5);
-        Position pos3 = new Position(4,5);
+    public void moveLeft(){
+        pos = pos.left();
+        Position expected = new Position(9,10);
+        assertEquals(expected, pos);
+    }
 
-        assertTrue(pos2.equals(pos3));
-        assertFalse(pos1.equals(pos2));
+    @Test
+    public void moveRight(){
+        pos = pos.right();
+        Position expected = new Position(11,10);
+        assertEquals(expected, pos);
+    }
+
+    @Test
+    public void moveUp(){
+        pos = pos.up();
+        Position expected = new Position(10,9);
+        assertEquals(expected, pos);
+
+    }
+    @Test
+    public void moveDown(){
+        pos = pos.down();
+        Position expected = new Position(10,11);
+        assertEquals(expected, pos);
+
     }
 }
