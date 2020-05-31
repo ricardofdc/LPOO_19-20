79cb297460f383c3044940a44f338d96fbad88c2
Marta Lobo <56152625+martandradelobo@users.noreply.github.com>
2020-05-31 17:56:45 +0100

    test arena functional

--- src/test/java/testArena.java
+++ src/test/java/testArena.java
@@ -1,37 +1,11 @@
-import Model.Position;
-import Model.Arena;
+import Model.*;
 import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class testArena {
-
-   /* @Test
-    public void createArena(){
-        int width = 50;
-        int height = 60;
-
-        ArenaCreator arena = new ArenaCreator();
-
-        Ship ship = new Ship(new Position(width / 2, height - 1));
-        Ball ball = new Ball(new Position(width/2, height-2));
-
-        Arena expected = new Arena(ship, ball, width, height, 1);
-
-        for(int i=0; i<width; i++){
-            expected.addWall(new Wall(new Position(i,0)));
-            expected.addWall(new Wall(new Position(i,height+1)));
-        }
-        for(int i=1; i<height; i++){
-            expected.addWall(new Wall(new Position(0,i)));
-            expected.addWall(new Wall(new Position(width+1,i)));
-        }
-
-        assertEquals(expected.getWalls(),arena.createArena(1).getWalls());
-    }
-*/
-
+    
     @Test
     public void testArenaInit()
     {
@@ -58,6 +32,17 @@ public class testArena {
         assertEquals(arena.getWidth(), 60);
         assertEquals(arena.getBall(), ball);
         assertEquals(arena.getShip(), ship);
+    }
 
+    @Test
+    public void testScore(){
+        Position pos = new Position(10, 10);
+        Ball ball = new Ball(pos);
+        Ship ship = new Ship(pos);
+        Arena arena = new Arena(ship, ball, 60, 35, 1);
+
+        arena.incrementScore(5);
+        assertEquals(arena.getScore(), 5);
     }
 }
+
